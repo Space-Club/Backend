46716654dbc181b14be74bb92033145675d5abab
junho <ssmm0205@naver.com>
2023-10-21 17:37:16 +0900

    feat: project setup

---
+++ src/main/java/com/spaceclub/SpaceClubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spaceclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceClubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceClubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spaceclub/SpaceClubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceClubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
