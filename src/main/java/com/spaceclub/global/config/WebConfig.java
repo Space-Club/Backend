19d37694ec49244d155862ff8f8679a246b19ac5
junho <ssmm0205@naver.com>
2023-10-23 22:39:35 +0900

    feat: cors 설정, 설정 시간 30분

---
+++ src/main/java/com/spaceclub/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.spaceclub.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(1800); // 1800초, 30분으로 설정
+    }
+
+}
