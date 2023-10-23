4257b22d0969a95f3cdc4ab3796adad63e296b56
junho <ssmm0205@naver.com>
2023-10-23 22:38:39 +0900

    feat: Spring Security csrf 및 cors 설정

---
+++ src/main/java/com/spaceclub/global/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.spaceclub.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+}
