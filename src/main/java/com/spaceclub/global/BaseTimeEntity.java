9eccc827c62c7f90abeff929cf0849bad75cde4f
hyeon-z <phj8221@naver.com>
2023-10-24 14:28:34 +0900

    refactor: 클래스 단에 붙인 @Getter 제거

--- src/main/java/com/spaceclub/global/BaseTimeEntity.java
+++ src/main/java/com/spaceclub/global/BaseTimeEntity.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
