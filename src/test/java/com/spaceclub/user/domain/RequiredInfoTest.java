466a63186b0ca892a2dd8d285dd7e962b5bc7d1b
junho <ssmm0205@naver.com>
2023-10-25 01:00:11 +0900

    test: 필수정보 검증에 대한 테스트 작성

---
+++ src/test/java/com/spaceclub/user/domain/RequiredInfoTest.java
@@ -0,0 +1,45 @@
+package com.spaceclub.user.domain;
+
+import com.spaceclub.SpaceClubCustomDisplayNameGenerator;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(SpaceClubCustomDisplayNameGenerator.ReplaceUnderscores.class)
+class RequiredInfoTest {
+
+    private final PhoneNumber validPhoneNumber = new PhoneNumber("010-1234-5678");
+
+    @ParameterizedTest(name = "{index}. value : {arguments}")
+    @NullSource
+    @ValueSource(strings = {"", " ", "    ", "a ", "a b", "    a   "})
+    void 닉네임이_null이거나_공백을_포함하면_예외를_발생한다(String invalidNickname) {
+        // when, then
+        assertThatThrownBy(() -> new RequiredInfo(invalidNickname, validPhoneNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "{index}. nickname : {arguments}")
+    @ValueSource(strings = {"1", "가", "가나다라마바사아자차카", "12345678901"})
+    void 닉네임이_2글자이상_10글자이하_범위에서_벗어나면_예외를_발생한다(String invalidNickname) {
+        // when, then
+        assertThatThrownBy(() -> new RequiredInfo(invalidNickname, validPhoneNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 닉네임이_2글자이상_10글자이하_범위에_있으면_예외를_발생하지_않는다() {
+        // given
+        String validNickname = "가나";
+
+        // when, then
+        assertThatNoException()
+                .isThrownBy(() -> new RequiredInfo(validNickname, validPhoneNumber));
+    }
+
+}
