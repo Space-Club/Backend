a065dbe2794518abfe4c2789ba41a31882124051
junho <ssmm0205@naver.com>
2023-10-25 02:01:49 +0900

    feat: jasypt 라이브러리 적용

---
+++ src/main/java/com/spaceclub/global/config/PropertiesConfig.java
@@ -0,0 +1,31 @@
+package com.spaceclub.global.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("develop")
+public class PropertiesConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);             //암호화, 복호화할 때 사용할 패스워드
+        config.setAlgorithm("PBEWithMD5AndDES");  //암호화 알고리즘
+        config.setKeyObtentionIterations("1000"); //해시 반복 횟수
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+
+}
---
+++ src/test/java/com/spaceclub/global/utils/EncryptionTest.java
@@ -0,0 +1,35 @@
+package com.spaceclub.global.utils;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+class EncryptionTest {
+
+    @Test
+    void jasypt를_통해_암호화에_성공한다() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+
+        encryptor.setPassword(""); // 암호화 및 복호화 시 사용하는 패스워드
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setStringOutputType("base64");
+
+        String url = "";
+        String username = "";
+        String password = "";
+        String urlResult = encryptor.encrypt(url);
+        String usernameResult = encryptor.encrypt(username);
+        String passwordReesult = encryptor.encrypt(password);
+
+        System.out.println("url plain : " + encryptor.decrypt(urlResult));          //복호화
+        System.out.println("url encoding : " + urlResult);                          //암호화 (설정파일에 사용)
+
+        System.out.println("username plain : " + encryptor.decrypt(usernameResult));//복호화
+        System.out.println("username encoding : " + usernameResult);                //암호화 (설정파일에 사용)
+
+        System.out.println("password plain : " + encryptor.decrypt(passwordReesult));//복호화
+        System.out.println("password encoding : " + passwordReesult);                //암호화 (설정파일에 사용)
+    }
+
+}
