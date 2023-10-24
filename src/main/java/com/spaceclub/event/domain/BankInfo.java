bde4548b1a0484aa21a67d4976c786eb24597e33
hyeon-z <phj8221@naver.com>
2023-10-24 10:30:17 +0900

    feat: 행사 및 행사에서 쓰이는 임베디드, Enum 클래스 생성

---
+++ src/main/java/com/spaceclub/event/domain/BankInfo.java
@@ -0,0 +1,17 @@
+package com.spaceclub.event.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BankInfo {
+
+    private String bankName;
+
+    private String accountNumber;
+
+}
