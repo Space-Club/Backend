4b47bd1635b70b6a3e3f5f4b45f8926b4a0adca6
Seongwon Choi <vlflalektzja@naver.com>
2023-10-24 17:59:20 +0900

    [SKRB-144] feat: 클럽 생성, 조회, 삭제 restdocs 작성 (#7)

---
+++ src/main/java/com/spaceclub/club/controller/ClubController.java
@@ -0,0 +1,34 @@
+package com.spaceclub.club.controller;
+
+import com.spaceclub.club.controller.dto.CreateClubRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClubController {
+
+    @PostMapping("/club")
+    public ResponseEntity<String> createClub(@RequestBody CreateClubRequest request) {
+        return ResponseEntity.created(URI.create("https://spaceclub.site/1")).build();
+    }
+
+    @GetMapping("/club/{clubId}")
+    public ResponseEntity<String> getClub(@PathVariable Long clubId) {
+        return ResponseEntity.ok("get club.");
+    }
+
+    @DeleteMapping("/club/{clubId}")
+    public ResponseEntity<String> deleteClub(@PathVariable Long clubId) {
+        return ResponseEntity.ok("delete club.");
+    }
+
+}
---
+++ src/test/java/com/spaceclub/club/controller/ClubControllerTest.java
@@ -0,0 +1,103 @@
+package com.spaceclub.club.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spaceclub.club.controller.dto.CreateClubRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ClubController.class)
+@AutoConfigureRestDocs
+class ClubControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    @DisplayName("클럽 생성에 성공한다")
+    @WithMockUser
+    void createClubTest() throws Exception {
+        // given
+        CreateClubRequest request = new CreateClubRequest("requiredInfo");
+
+        // when
+        ResultActions result = this.mockMvc.perform(post("/api/v1/club")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isCreated())
+                .andDo(print())
+                .andDo(document("club/create",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("requiredInfo").type(JsonFieldType.STRING).description("클럽 생성시 필요한 임시 정보")
+                        )));
+    }
+
+    @Test
+    @DisplayName("클럽 조회에 성공한다")
+    @WithMockUser
+    void getClubTest() throws Exception {
+        // given
+        Long clubId = 1L;
+
+        // when
+        ResultActions result = this.mockMvc.perform(get("/api/v1/club/{clubId}", clubId)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("club/get",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    @DisplayName("클럽 삭제에 성공한다")
+    @WithMockUser
+    void deleteClubTest() throws Exception {
+        // given
+        Long clubId = 1L;
+
+        // when
+        ResultActions result = this.mockMvc.perform(delete("/api/v1/club/{clubId}", clubId)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("club/delete",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
+
+}
