bcbb4fddd7ae6c3daaa2456f8a0e1e460158ac5a
hyeon-z <phj8221@naver.com>
2023-10-26 20:35:58 +0900

    fix: 암호화 관련 @Profile설정에 local환경 추가

--- src/main/java/com/spaceclub/global/config/PropertiesConfig.java
+++ src/main/java/com/spaceclub/global/config/PropertiesConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("develop")
+@Profile({"local", "develop"})
 @Configuration
 public class PropertiesConfig {
 
