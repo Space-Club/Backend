fba48cd535abd56460650661b4970174a92f0a8f
hyeon-z <phj8221@naver.com>
2023-10-26 19:45:20 +0900

    refactor: 행사 생성 api를 multipart/form-data로 변경

--- src/main/java/com/spaceclub/event/controller/EventController.java
+++ src/main/java/com/spaceclub/event/controller/EventController.java
@@ -2,13 +2,17 @@ package com.spaceclub.event.controller;
 
 import com.spaceclub.event.controller.dto.CreateEventRequest;
 import com.spaceclub.event.service.EventService;
+import com.spaceclub.global.S3ImageUploader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
 
 @Controller
@@ -18,11 +22,15 @@ public class EventController {
 
     private final EventService eventService;
 
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody CreateEventRequest request) {
-        Long eventId = eventService.create(request.toEntity());
+    private final S3ImageUploader uploader;
 
-        return ResponseEntity.created(URI.create("https/" + eventId)).build();
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> create(@RequestPart CreateEventRequest request, @RequestPart MultipartFile poster) throws IOException {
+
+        String fileName = uploader.uploadImage(poster);
+        Long eventId = eventService.create(request.toEntity(fileName));
+
+        return ResponseEntity.created(URI.create("/api/v1/event/" + eventId)).build();
     }
 
 }
