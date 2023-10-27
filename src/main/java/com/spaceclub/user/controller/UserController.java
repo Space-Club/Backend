39dbfa4dd486b0c2ce369d01d79c4f0b1f205a1d
juno-junho <ssmm0205@naver.com>
2023-10-27 14:42:29 +0900

    chore: endpoint 누락 값 추가

--- src/main/java/com/spaceclub/user/controller/UserController.java
+++ src/main/java/com/spaceclub/user/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
