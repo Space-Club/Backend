23560e066d9f2a5e9c52a072ee0e1a836fbef47d
juno-junho <ssmm0205@naver.com>
2023-10-30 01:11:04 +0900

    feat: event status를 찾아오는 service 메서드 추가

--- src/main/java/com/spaceclub/user/service/UserService.java
+++ src/main/java/com/spaceclub/user/service/UserService.java
@@ -1,15 +1,28 @@
 package com.spaceclub.user.service;
 
 import com.spaceclub.event.domain.Event;
+import com.spaceclub.event.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final EventRepository eventRepository;
+
     public Page<Event> findAllEventPages(Long userId, Pageable pageable) {
+//        return eventRepository.findAllByUserId(userId, pageable);
+        return null;
+    }
+
+    public String findEventStatus(Long userId, Event event) {
+//        return eventRepository.findEventStatusByUserId(userId, event);
         return null;
     }
 
 }
+
+
