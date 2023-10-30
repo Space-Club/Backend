0f8d4d2c196df23a049cf75cef0169ec1b6c7c60
juno-junho <ssmm0205@naver.com>
2023-10-30 18:00:57 +0900

    feat: 유저가 신청한 행사 조회 기능 구현

--- src/main/java/com/spaceclub/user/service/UserService.java
+++ src/main/java/com/spaceclub/user/service/UserService.java
@@ -1,7 +1,7 @@
 package com.spaceclub.user.service;
 
 import com.spaceclub.event.domain.Event;
-import com.spaceclub.event.repository.EventRepository;
+import com.spaceclub.event.repository.EventUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,16 +11,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final EventRepository eventRepository;
+    private final EventUserRepository eventUserRepository;
 
     public Page<Event> findAllEventPages(Long userId, Pageable pageable) {
-//        return eventRepository.findAllByUserId(userId, pageable);
-        return null;
+        return eventUserRepository.findAllByUserId(userId, pageable);
     }
 
-    public String findEventStatus(Long userId, Event event) {
-//        return eventRepository.findEventStatusByUserId(userId, event);
-        return null;
+    public String findEventStatus(Long userId, Event event){
+        return eventUserRepository.findEventStatusByUserId(userId, event);
     }
 
 }
