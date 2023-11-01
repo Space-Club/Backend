95a1f0b42013ebaf81ab515654bd112dae7a5bd9
hyeon-z <phj8221@naver.com>
2023-11-01 23:58:19 +0900

    test: ClubUserRepositoryTest의 컨텍스트 분리 설정

--- src/test/java/com/spaceclub/club/repository/ClubUserRepositoryTest.java
+++ src/test/java/com/spaceclub/club/repository/ClubUserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Optional;
 
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
+@DirtiesContext
 @DisplayNameGeneration(SpaceClubCustomDisplayNameGenerator.class)
 class ClubUserRepositoryTest {
 
