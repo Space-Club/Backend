c075ab17666173fc33910047a8b54a6213f38bac
hyeon-z <phj8221@naver.com>
2023-11-01 12:39:04 +0900

    feat: Event, Club 엔티티에 EqualsAndHashcode 추가

--- src/main/java/com/spaceclub/event/domain/Event.java
+++ src/main/java/com/spaceclub/event/domain/Event.java
@@ -14,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,6 +27,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
 public class Event extends BaseTimeEntity {
 
     @Id
@@ -109,6 +111,10 @@ public class Event extends BaseTimeEntity {
 
     public String getLocation() {
         return eventInfo.getLocation();
+    }
+
+    public Long getClubId() {
+        return club.getId();
     }
 
     public String getClubName() {
