ee046ffd9d24fbe71c1ffe538035d02428737cf3
juno-junho <ssmm0205@naver.com>
2023-11-02 19:40:59 +0900

    feat: oauth 설정 및 dto 생성

---
+++ src/main/java/com/spaceclub/global/oauth/service/vo/KakaoTokenInfo.java
@@ -0,0 +1,15 @@
+package com.spaceclub.global.oauth.service.vo;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = SnakeCaseStrategy.class)
+public record KakaoTokenInfo(
+        String tokenType,
+        String accessToken,
+        Integer expiresIn,
+        String refreshToken,
+        Integer refreshTokenExpiresIn
+) {
+
+}
