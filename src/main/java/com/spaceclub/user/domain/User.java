20775de20e60aed5bf283d67f85aa407a6fd6ce5
juno-junho <ssmm0205@naver.com>
2023-11-02 20:11:58 +0900

    feat: user 도메인 생성자 변경 및 기본 값 설정

--- src/main/java/com/spaceclub/user/domain/User.java
+++ src/main/java/com/spaceclub/user/domain/User.java
@@ -30,6 +30,9 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @Column(length = 100)
+    private String password;
+
     @Embedded
     private RequiredInfo requiredInfo;
 
