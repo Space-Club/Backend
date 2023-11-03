b03348a9270e7c09425c28628af66762234cb260
juno-junho <ssmm0205@naver.com>
2023-11-03 15:27:11 +0900

    feat: 카카오 OAuth 회원가입 기능 구현

--- src/main/java/com/spaceclub/user/controller/UserController.java
+++ src/main/java/com/spaceclub/user/controller/UserController.java
@@ -2,14 +2,21 @@ package com.spaceclub.user.controller;
 
 import com.spaceclub.event.domain.Event;
 import com.spaceclub.global.dto.PageResponse;
+import com.spaceclub.global.oauth.service.OAuthService;
+import com.spaceclub.global.oauth.service.vo.KakaoTokenInfo;
+import com.spaceclub.global.oauth.service.vo.KakaoUserInfo;
 import com.spaceclub.user.controller.dto.UserEventGetResponse;
+import com.spaceclub.user.controller.dto.UserLoginResponse;
+import com.spaceclub.user.domain.User;
 import com.spaceclub.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,6 +29,7 @@ import static com.spaceclub.user.controller.dto.UserEventGetResponse.from;
 public class UserController {
 
     private final UserService userService;
+    private final OAuthService oAuthService;
 
     @GetMapping("/{userId}/events")
     public PageResponse<UserEventGetResponse, Event> getAllEvents(@PathVariable Long userId, Pageable pageable) {
@@ -32,6 +40,22 @@ public class UserController {
                 .toList();
 
         return new PageResponse<>(eventGetResponse, eventPages);
+    }
+
+    @PostMapping("/oauths")
+    public UserLoginResponse getKaKaoCode(@RequestParam String code) {
+        User user = createKakaoUser(code);
+        userService.save(user);
+        boolean newMember = userService.isNewMember(user);
+
+        return new UserLoginResponse("accessToken", newMember);
+    }
+
+    private User createKakaoUser(String code) {
+        KakaoTokenInfo accessTokenInfo = oAuthService.getAccessTokenInfo(code);
+        String accessToken = accessTokenInfo.accessToken();
+        KakaoUserInfo userInfo = oAuthService.getUserInfo(accessToken);
+        return userInfo.toUser();
     }
 
 }
--- src/main/java/com/spaceclub/user/service/UserService.java
+++ src/main/java/com/spaceclub/user/service/UserService.java
@@ -2,16 +2,21 @@ package com.spaceclub.user.service;
 
 import com.spaceclub.event.domain.Event;
 import com.spaceclub.event.repository.EventUserRepository;
+import com.spaceclub.user.domain.User;
+import com.spaceclub.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
     private final EventUserRepository eventUserRepository;
+    private final UserRepository userRepository;
 
     public Page<Event> findAllEventPages(Long userId, Pageable pageable) {
         return eventUserRepository.findAllByUserId(userId, pageable);
@@ -19,6 +24,18 @@ public class UserService {
 
     public String findEventStatus(Long userId, Event event){
         return eventUserRepository.findEventStatusByUserId(userId, event);
+    }
+
+    public boolean isNewMember(User user) {
+        Long userId = user.getId();
+        return userRepository.findById(userId)
+                .orElseThrow()
+                .isNewMember();
+    }
+
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
     }
 
 }
