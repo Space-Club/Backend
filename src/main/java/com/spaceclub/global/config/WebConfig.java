3d329de9f21b5932515fdb4b6ba28f30ffd10ae1
Seongwon Choi <vlflalektzja@naver.com>
2023-11-06 14:01:45 +0900

    Location 포함 헤더 노출 설정 (#86)

--- src/main/java/com/spaceclub/global/config/WebConfig.java
+++ src/main/java/com/spaceclub/global/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173", "https://spaceclub.vercel.app", "https://spaceclub.site")
                 .allowedMethods("*")
                 .allowCredentials(true)
+                .exposedHeaders("*")
                 .maxAge(1800); // 1800초, 30분으로 설정
     }
 
