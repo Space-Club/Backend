ab954afb9333753f9a89eee3d0d4b85f2d3b3b90
juno-junho <ssmm0205@naver.com>
2023-11-14 16:40:22 +0900

    feat: S3ImageUploader 예외 변환 및 중복 코드 리팩토링

--- src/main/java/com/spaceclub/global/S3ImageUploader.java
+++ src/main/java/com/spaceclub/global/S3ImageUploader.java
@@ -18,6 +18,7 @@ import java.util.Date;
 public class S3ImageUploader {
 
     private static final String DOT = ".";
+    public static final String DATE_FORMAT = "yyyyMMdd_HHmmss";
 
     private final AmazonS3Client amazonS3Client;
 
@@ -27,40 +28,44 @@ public class S3ImageUploader {
     @Value("${s3.folder.name.club-logo}")
     private String clubLogoFolder;
 
-    public String uploadPosterImage(MultipartFile posterImage) throws IOException {
-        String fileName = createFileName(posterImage.getOriginalFilename());
+    @Value("${s3.folder.name.user-profile-image}")
+    private String userProfileImageFolder;
 
-        ObjectMetadata objectMetaData = new ObjectMetadata();
-
-        objectMetaData.setContentType(posterImage.getContentType());
-        objectMetaData.setContentLength(posterImage.getSize());
-
-        amazonS3Client.putObject(
-                new PutObjectRequest(eventPosterFolder, fileName, posterImage.getInputStream(), objectMetaData)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)
-        );
-
-        return fileName;
+    public String uploadPosterImage(MultipartFile posterImage) {
+        return upload(posterImage, eventPosterFolder);
     }
 
-    public String uploadClubLogoImage(MultipartFile logoImage) throws IOException {
-        String fileName = createFileName(logoImage.getOriginalFilename());
+    public String uploadClubLogoImage(MultipartFile logoImage) {
+        return upload(logoImage, clubLogoFolder);
+    }
 
+    public String uploadUserProfileImage(MultipartFile userImage) {
+        return upload(userImage, userProfileImageFolder);
+    }
+
+    private String upload(MultipartFile userImage, String userProfileImageFolder) {
+        String originalFilename = userImage.getOriginalFilename();
+        if (originalFilename == null) throw new IllegalArgumentException("파일 이름이 존재하지 않습니다.");
+
+        String fileName = createFileName(originalFilename);
         ObjectMetadata objectMetaData = new ObjectMetadata();
 
-        objectMetaData.setContentType(logoImage.getContentType());
-        objectMetaData.setContentLength(logoImage.getSize());
+        objectMetaData.setContentType(userImage.getContentType());
+        objectMetaData.setContentLength(userImage.getSize());
 
-        amazonS3Client.putObject(
-                new PutObjectRequest(clubLogoFolder, fileName, logoImage.getInputStream(), objectMetaData)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)
-        );
+        try {
+            amazonS3Client.putObject(
+                    new PutObjectRequest(userProfileImageFolder, fileName, userImage.getInputStream(), objectMetaData)
+                            .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new IllegalStateException("파일 업로드에 실패했습니다.");
+        }
 
         return fileName;
     }
 
     private String createFileName(String originalName) {
-        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timestamp = new SimpleDateFormat(DATE_FORMAT).format(new Date());
 
         int lastDot = originalName.lastIndexOf(DOT);
         String fileName = originalName.substring(0, lastDot);
