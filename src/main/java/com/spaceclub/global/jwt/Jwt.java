63f1c7caa77dba61f599c808d5d117ccac561aca
juno-junho <ssmm0205@naver.com>
2023-11-18 17:36:11 +0900

    feat: jwt refresh token 검증 기능 구현

--- src/main/java/com/spaceclub/global/jwt/Jwt.java
+++ src/main/java/com/spaceclub/global/jwt/Jwt.java
@@ -1,18 +1,22 @@
 package com.spaceclub.global.jwt;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 
+@Slf4j
 public class Jwt {
 
     private final String issuer;
 
     private final int expirySeconds;
+
+    private final int refreshTokenExpirySeconds;
 
     private final Algorithm algorithm;
 
@@ -22,31 +26,54 @@ public class Jwt {
     private Jwt(
             String issuer,
             String clientSecret,
-            int expirySeconds
-    ) {
+            int expirySeconds,
+            int refreshTokenExpirySeconds) {
         this.issuer = issuer;
         this.expirySeconds = expirySeconds;
         this.algorithm = Algorithm.HMAC512(clientSecret);
+        this.refreshTokenExpirySeconds = refreshTokenExpirySeconds;
         this.jwtVerifier = JWT.require(algorithm)
                 .withIssuer(issuer)
                 .build();
     }
 
-     public String sign(Claims claims) { // 토큰 생성
+     public String signAccessToken(Claims claims) {
          Date now = new Date();
-         JWTCreator.Builder builder = JWT.create();
-         builder.withIssuer(issuer);
-         builder.withIssuedAt(now);
-         if (expirySeconds > 0) {
-             builder.withExpiresAt(new Date(now.getTime() + (expirySeconds * 1_000L)));
-         }
-         builder.withClaim(Claims.USER_ID, claims.getId());
-         builder.withClaim(Claims.USER_NAME, claims.getUsername());
-         return builder.sign(algorithm);
+
+         return JWT.create()
+                 .withIssuer(issuer)
+                 .withIssuedAt(now)
+                 .withExpiresAt(new Date(now.getTime() + (expirySeconds * 1_000L)))
+                 .withClaim(Claims.USER_ID, claims.getId())
+                 .withClaim(Claims.USER_NAME, claims.getUsername())
+                 .sign(algorithm);
      }
 
-     public Claims verify(String token) { // 디코딩
-        return new Claims(jwtVerifier.verify(token));
-     }
+    public String signRefreshToken() {
+        Date now = new Date();
+
+        return JWT.create()
+                .withIssuer(issuer)
+                .withIssuedAt(now)
+                .withExpiresAt(new Date(now.getTime() + refreshTokenExpirySeconds * 1_000L))
+                .sign(algorithm);
+    }
+
+    public boolean isValidFormat(String token) { // 디코딩
+        try {
+            jwtVerifier.verify(token);
+            return true;
+        } catch (TokenExpiredException e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return new Claims(JWT.decode(token));
+    }
+
+    public void verify(String token) {
+        jwtVerifier.verify(token);
+    }
 
 }
