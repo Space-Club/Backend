1bec190239ea4c5ad225748b60dc02a8bb3f48e1
Seongwon Choi <vlflalektzja@naver.com>
2023-11-21 14:26:13 +0900

    클럽 로고 이미지 저장시 URL prefix 제거 (#179)

--- src/main/java/com/spaceclub/club/controller/ClubController.java
+++ src/main/java/com/spaceclub/club/controller/ClubController.java
@@ -40,7 +40,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
