4b9812caf45ebe584b25a93f6c2b5f12a9114b8e
Park Hyeon Ji <63945197+hyeon-z@users.noreply.github.com>
2023-11-22 16:49:26 +0900

    행사 참여 상태 변수명 participationStatus으로 통일 (#198)

--- src/main/java/com/spaceclub/form/controller/dto/FormSubmitUpdateRequest.java
+++ src/main/java/com/spaceclub/form/controller/dto/FormSubmitUpdateRequest.java
@@ -2,6 +2,6 @@ package com.spaceclub.form.controller.dto;
 
 import com.spaceclub.event.domain.ParticipationStatus;
 
-public record FormSubmitUpdateRequest(Long formUserId, ParticipationStatus status) {
+public record FormSubmitUpdateRequest(Long formUserId, ParticipationStatus participationStatus) {
 
 }
