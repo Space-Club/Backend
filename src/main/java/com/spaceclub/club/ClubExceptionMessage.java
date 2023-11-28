a389ac9167f45d335e75e5ba777ce46b089989e5
Choi Seongwon <vlflalektzja@naver.com>
2023-11-28 17:31:25 +0900

    feat: 클럽 공지사항 유효성 검사

--- src/main/java/com/spaceclub/club/ClubExceptionMessage.java
+++ src/main/java/com/spaceclub/club/ClubExceptionMessage.java
@@ -10,6 +10,9 @@ public enum ClubExceptionMessage implements ExceptionMessageInterface {
 
     CLUB_NOT_FOUND("존재하지 않는 클럽입니다"),
     NOTICE_NOT_FOUND("존재하지 않는 공지사항입니다"),
+    NOTICE_NOT_NULL("공지사항은 NULL일 수 없습니다"),
+    NOTICE_WITH_BLANK("공지사항은 빈 칸일 수 없습니다"),
+    NOTICE_WITH_MARGIN("공지사항의 앞뒤에 공백이 올 수 없습니다"),
     CAN_NOT_SELF_DEGRADING("혼자 남은 관리자는 강등될 수 없습니다"),
     CLUB_ALREADY_JOINED("이미 해당 클럽에 가입되어 있습니다"),
     NOT_CLUB_MEMBER("해당 클럽의 멤버가 아닙니다"),
