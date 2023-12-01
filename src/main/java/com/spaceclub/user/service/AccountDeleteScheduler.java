1836b3b194a999cadcc1f435e4df2fe7033e8b15
juno-junho <ssmm0205@naver.com>
2023-12-01 18:42:56 +0900

    fix: 유저 삭제시 NPE 해결

--- src/main/java/com/spaceclub/user/service/AccountDeleteScheduler.java
+++ src/main/java/com/spaceclub/user/service/AccountDeleteScheduler.java
@@ -4,6 +4,7 @@ import com.spaceclub.global.config.oauth.KakaoOauthInfoSender;
 import com.spaceclub.user.domain.User;
 import com.spaceclub.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AccountDeleteScheduler {
@@ -26,11 +28,12 @@ public class AccountDeleteScheduler {
 //    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
     @Scheduled(fixedRate = 1000 * 60) // 테스트
     public void deleteInactiveUsers() {
+        log.warn("deleteInactiveUsers() is called");
 //        LocalDateTime threeDaysAgoFromNow = LocalDateTime.now().minusDays(GRACE_DAYS_OF_DELETION);
         LocalDateTime threeDaysAgoFromNow = LocalDateTime.now().minusMinutes(GRACE_DAYS_OF_DELETION);
         List<User> usersToDelete = userRepository.findAllUserToDelete(threeDaysAgoFromNow).stream()
-                .map(User::changeStatusToDeleted)
                 .peek(kakaoOauthInfoSender::unlink)
+                .map(User::changeStatusToDeleted)
                 .toList();
 
         userRepository.saveAll(usersToDelete);
