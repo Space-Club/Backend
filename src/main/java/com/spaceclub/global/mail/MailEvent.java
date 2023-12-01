64896f72b77e6be9034e25d01c69e4123eb0a912
Junho Hwang <72647031+juno-junho@users.noreply.github.com>
2023-12-01 16:18:39 +0900

    회원 가입 시 웰컴 이메일 보내는 기능 구현 (#274)

--- src/main/java/com/spaceclub/global/mail/MailEvent.java
+++ src/main/java/com/spaceclub/global/mail/MailEvent.java
@@ -1,5 +1,18 @@
 package com.spaceclub.global.mail;
 
+import java.util.List;
+
 public record MailEvent(MailInfo mailInfo) {
 
+    public static MailEvent welcomeEvent(String email) {
+        MailInfo mailInfo = MailInfo.of(
+                List.of(email),
+                List.of(),
+                "Space Club에 가입해주셔서 감사합니다.",
+                "welcome",
+                "index"
+        );
+
+        return new MailEvent(mailInfo);
+    }
 }
